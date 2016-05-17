c5e3ef175b8a177068a35830961ef59509ad4b48
bboxhe <bboxhe@gmail.com>
2016-05-17 19:33:59 +0800

    rmmove invalid file

--- HP P1108最简移植/ghostscript/ghostscript-9.14/base/cp.cmd
+++
@@ -1,2 +0,0 @@
-@copy /B %1 %2
-@if not "%2"=="." touch %2
--- HP P1108最简移植/ghostscript/ghostscript-9.14/lib/gssetgs.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-rem Set default values for GS (gs with graphics window) and GSC
-rem (console mode gs) if the user hasn't set them.
-
-if %GS%/==/ set GS=gswin32
-if %GSC%/==/ set GSC=gswin32c
