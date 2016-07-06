5db1e48bdf3ba178121fed7105bbdcbc32fcf5ba
bboxhe <bboxhe@gmail.com>
2016-07-06 12:19:02 +0800

    update

--- localprint/src/main/java/com/github/openthos/printer/localprint/service/LocalPrintService.java
+++ localprint/src/main/java/com/github/openthos/printer/localprint/service/LocalPrintService.java
@@ -110,66 +110,7 @@ public class LocalPrintService extends Service {
      *显示通知栏
      */
     private void showNotification() {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
 
-        RemoteViews views = new RemoteViews(getPackageName(), R.layout.notification_jobs);
-
-        Intent intent = new Intent(this, JobManagerActivity.class);
-        PendingIntent pIntent = PendingIntent.getActivity(this, PendingIntent.FLAG_UPDATE_CURRENT, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        views.setOnClickPendingIntent(R.id.button, pIntent);
-
-        //添加打印任务项
-
-        RemoteViews item1 = new RemoteViews(getPackageName(), R.layout.notification_item_jobs);
-        item1.setTextViewText(R.id.textview_name, "HP P1108");
-        views.addView(R.id.linearlayout_items, item1);
-
-        RemoteViews item2 = new RemoteViews(getPackageName(), R.layout.notification_item_jobs);
-        item2.setTextViewText(R.id.textview_name, "docu.pdf");
-        item2.setTextViewText(R.id.textview_status, "printing");
-        views.addView(R.id.linearlayout_items, item2);
-
-        //添加完毕
-
-        builder.setContent(views);
-        builder.setSmallIcon(R.mipmap.ic_launcher);
-        builder.setContentTitle(getString(R.string.print_info));
-        builder.setOngoing(true);
-
-        android.support.v4.app.NotificationCompat.InboxStyle inboxBuilder = new NotificationCompat.InboxStyle();
-        inboxBuilder.addLine("Test1");
-
-        builder.setStyle(inboxBuilder);
-
-        NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        mNotificationManager.notify(APP.NOTIFY_JOBS_ID, builder.build());
-
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(
-                this, 0, intent,
-                PendingIntent.FLAG_CANCEL_CURRENT);
-        Notification noti = new NotificationCompat.Builder(
-                this)
-                .setNumber(13)
-                .setContentIntent(pendingIntent)
-                .setStyle(
-                        new NotificationCompat.InboxStyle()
-                                .addLine(
-                                        "M.Twain (Google+) Haiku is more than a cert...")
-                                .addLine("M.Twain Reminder")
-                                .addLine("M.Twain Lunch?")
-                                .addLine("M.Twain Revised Specs")
-                                .addLine("M.Twain ")
-                                .addLine(
-                                        "Google Play Celebrate 25 billion apps with Goo..")
-                                .addLine(
-                                        "Stack Exchange StackOverflow weekly Newsl...")
-                                .setBigContentTitle("6 new message")
-                                .setSummaryText("mtwain@android.com"))
-                .build();
-        NotificationManager m1NotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        m1NotificationManager.notify(0, noti);
     }
 
     @Nullable
