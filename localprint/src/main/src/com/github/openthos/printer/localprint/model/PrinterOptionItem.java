ea9982dfc1ef576b40cbc21f4d380d5b8ac14360
Taocr <taocr2005@163.com>
2016-08-02 15:39:35 +0800

    add more paper MediaSize support

--- localprint/src/main/src/com/github/openthos/printer/localprint/model/PrinterOptionItem.java
+++ localprint/src/main/src/com/github/openthos/printer/localprint/model/PrinterOptionItem.java
@@ -196,19 +196,107 @@ public class PrinterOptionItem {
         // TODO: 2016/7/7 Add more mediaSize.
 
         String result = "A4";
-
-        if (mediaSize.equals(PrintAttributes.MediaSize.NA_LETTER)) {
-            result = "Letter";
-        } else if (mediaSize.equals(PrintAttributes.MediaSize.ISO_A4)) {
-            result = "A4";
-        } else if (mediaSize.equals(PrintAttributes.MediaSize.ISO_A5)) {
-            result = "A5";
-        } else if (mediaSize.equals(PrintAttributes.MediaSize.ISO_A6)) {
-            result = "A6";
-        } else if (mediaSize.equals(PrintAttributes.MediaSize.ISO_B5)) {
-            result = "B5";
-        } else if (mediaSize.equals(PrintAttributes.MediaSize.NA_MONARCH)) {
-            result = "Executive";
+        switch (mediaSize.getId()) {
+            //  if (mediaSize.equals(PrintAttributes.MediaSize.NA_LETTER.getId())) {
+            case "NA_LETTER":
+                result = "Letter";
+                break;
+            case "ISO_A2":
+                result = "A2";
+                break;
+            case "ISO_A3":
+                result = "A3";
+                break;
+            //} else if (mediaSize.equals(PrintAttributes.MediaSize.ISO_A4)) {
+            case "ISO_A4":
+                result = "A4";
+                //} else if (mediaSize.equals(PrintAttributes.MediaSize.ISO_A5)) {
+            case "ISO_A5":
+                result = "A5";
+                break;
+            //} else if (mediaSize.equals(PrintAttributes.MediaSize.ISO_A6)) {
+            case "ISO_A6":
+                result = "A6";
+                break;
+            //} else if (mediaSize.equals(PrintAttributes.MediaSize.ISO_B5)) {
+            case "ISO_B5":
+                result = "B5";
+                break;
+            case "ISO_B6":
+                result = "B6";
+                break;
+            case "ISO_B7":
+                result = "B7";
+                break;
+            case "ISO_C4":
+                result = "C4";
+                break;
+            case "ISO_C5":
+                result = "C5";
+            //} else if (mediaSize.equals(PrintAttributes.MediaSize.NA_MONARCH)) {
+            case "NA_MONARCH":
+                result = "Executive";
+                break;
+            case "ROC_8K":
+                result = "8k";
+                break;
+            case "ROC_16K":
+                result = "16k";
+                break;
+            case "NA_LEGAL":
+                result = "Legal";
+                break;
+            case "NA_LEDGER":
+                result = "Ledger";
+                break;
+            case "NA_TABLOID":
+                result = "B";
+                break;
+            case "NA_INDEX_3X5":
+                result = "Card3x5";
+                break;
+            case "JPN_HAGAKI":
+                result = "Hagaki";
+                break;
+            case "NA_INDEX_4X6":
+                result = "Photo4x6";
+                break;
+            case "NA_INDEX_5X8":
+                result = "Card5x8";
+                break;
+            case "JPN_OUFUKU":
+                result = "Oufuku";
+                break;
+            case "JIS_B5":
+                result = "JB5";
+                break;
+            case "JIS_B7":
+                result = "JB7";
+                break;
+            case "JIS_EXEC":
+                result = "ExecutiveJIS";
+                break;
+            case "JPN_CHOU2":
+                result = "EnvA2";
+                break;
+            case "JPN_CHOU3":
+                result = "EnvCHOU3";
+                break;
+            case "JPN_CHOU4":
+                result = "EnvCHOU4";
+                break;
+            case "PRC_5":
+                result = "EnvDL";
+                break;
+            case "NA_QUARTO":
+                result = "Mutsugiri";
+                break;
+            case "JPN_KAKU2":
+                result = "EnvKaku2";
+                break;
+            case "JPN_YOU4":
+                result = "Yougata4";
+                break;
         }
         return result;
     }
@@ -221,20 +309,126 @@ public class PrinterOptionItem {
      */
     public static PrintAttributes.MediaSize cups2media(String mediaSizeCupsItem) {
         PrintAttributes.MediaSize result = null;
-        if (mediaSizeCupsItem.equals("Letter")) {
-            result = PrintAttributes.MediaSize.NA_LETTER;
-        } else if (mediaSizeCupsItem.equals("A4")) {
-            result = PrintAttributes.MediaSize.ISO_A4;
-        } else if (mediaSizeCupsItem.equals("A5")) {
-            result = PrintAttributes.MediaSize.ISO_A5;
-        } else if (mediaSizeCupsItem.equals("A6")) {
-            result = PrintAttributes.MediaSize.ISO_A6;
-        } else if (mediaSizeCupsItem.equals("B5")) {
-            result = PrintAttributes.MediaSize.ISO_B5;
-        } else if (mediaSizeCupsItem.equals("Executive")) {
-            result = PrintAttributes.MediaSize.NA_MONARCH;
-        }
 
+        switch(mediaSizeCupsItem) {
+            case "Letter":
+                result = PrintAttributes.MediaSize.NA_LETTER;
+                break;
+            case "A2":
+                result = PrintAttributes.MediaSize.ISO_A2;
+                break;
+            case "A3":
+                result = PrintAttributes.MediaSize.ISO_A3;
+                break;
+            case "A4":
+                result = PrintAttributes.MediaSize.ISO_A4;
+                break;
+            case "A5":
+                result = PrintAttributes.MediaSize.ISO_A5;
+                break;
+            case "A6":
+            //case "A5Card":
+                result = PrintAttributes.MediaSize.ISO_A6;
+                break;
+            case "B5":
+            //case"ISOB5":
+            //case "B5iso":
+                result = PrintAttributes.MediaSize.ISO_B5;
+                break;
+            case "B6":
+                result = PrintAttributes.MediaSize.ISO_B6;
+                break;
+            case "B7":
+                result = PrintAttributes.MediaSize.ISO_B7;
+                break;
+            case "C4":
+                result = PrintAttributes.MediaSize.ISO_C4;
+                break;
+            case "C5":
+                result = PrintAttributes.MediaSize.ISO_C5;
+                break;
+            case "Executive":
+                result = PrintAttributes.MediaSize.NA_MONARCH;
+                break;
+            case "8k":
+            //case "270x390mm":
+                result = PrintAttributes.MediaSize.ROC_8K;
+                break;
+            case "16k":
+            //case "16k195x270":
+            //case "195x270mm":
+            //case "Big16K":
+                result = PrintAttributes.MediaSize.ROC_16K;
+                break;
+            case "Legal":
+                result = PrintAttributes.MediaSize.NA_LEGAL;
+                break;
+            case "Ledger":
+                result = PrintAttributes.MediaSize.NA_LEDGER;
+                break;
+            case "B":
+                result = PrintAttributes.MediaSize.NA_TABLOID;
+                break;
+            case "Card3x5":
+            //case "3x5in":
+                result = PrintAttributes.MediaSize.NA_INDEX_3X5;
+                break;
+            case "Hagaki":
+            //case "Postcard":
+            //case "100x148mm":
+                result = PrintAttributes.MediaSize.JPN_HAGAKI;
+                break;
+            case "Photo4x6":
+            //case "4x6in":
+                result = PrintAttributes.MediaSize.NA_INDEX_4X6;
+                break;
+            case "Card5x8":
+            //case "5x8in":
+                result = PrintAttributes.MediaSize.NA_INDEX_5X8;
+                break;
+            case "Oufuku":
+            //case "JapanesePostcard":
+                result = PrintAttributes.MediaSize.JPN_OUFUKU;
+                break;
+            case "JB5":
+            //case "B5jis":
+            //case "JISB5":
+                result = PrintAttributes.MediaSize.JIS_B5;
+                break;
+            case "JB7":
+                result = PrintAttributes.MediaSize.JIS_B7;
+                break;
+            case "ExecutiveJIS":
+            //case "FLSA":
+                result = PrintAttributes.MediaSize.JIS_EXEC;
+                break;
+            case "EnvA2":
+                result = PrintAttributes.MediaSize.JPN_CHOU2;
+                break;
+            case "EnvChou3":
+            //case "Younaga3":
+            //case "Nagagata3":
+                result = PrintAttributes.MediaSize.JPN_CHOU3;
+                break;
+            case "EnvChou4":
+                result = PrintAttributes.MediaSize.JPN_CHOU4;
+                break;
+            case "EnvDL":
+            //case "ENVELOPEDL":
+                result = PrintAttributes.MediaSize.PRC_5;
+                break;
+            case "Mutsugiri":
+            //case "8x10in":
+                result = PrintAttributes.MediaSize.NA_QUARTO;
+                break;
+            case "EnvKaku2":
+                result = PrintAttributes.MediaSize.JPN_KAKU2;
+                break;
+            case "Yougata4":
+                result = PrintAttributes.MediaSize.JPN_YOU4;
+                break;
+
+        }
         return result;
     }
 
