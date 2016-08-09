4caabbe82be704bb2a6ade9fb20f8a308bbd1477
Taocr <taocr2005@163.com>
2016-08-09 13:53:45 +0800

    Add RepairPdfTask file

---
+++ localprint/src/main/src/com/github/openthos/printer/localprint/task/RepairPdfTask.java
@@ -0,0 +1,25 @@
+package com.github.openthos.printer.localprint.task;
+
+import java.util.List;
+
+/**
+ * Created by Taocr on 2016/8/8.
+ */
+public class RepairPdfTask extends CommandTask <String, Void, Void> {
+    @Override
+    protected String[] setCmd(String... params) {
+        String fileName = params[0];
+        return new String[]{"sh","proot.sh","gs","-o",fileName + "1","-sDEVICE=pdfwrite",fileName};
+    }
+
+    @Override
+    protected Void handleCommand(List stdOut, List stdErr) {
+        return null;
+    }
+
+    @Override
+    protected String bindTAG() {
+        return "RepairPdfTask";
+    }
+
+}
